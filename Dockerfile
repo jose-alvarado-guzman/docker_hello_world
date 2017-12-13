4fbee8910ed22e540856d6a3f9bc6f04336dd942
Jose A Alvarado <jaa2220@cumc.columbia.edu>
2017-12-13 17:37:53 -0500

    First commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+from python:3.6.2
+
+WORKDIR /app
+
+ADD . /app
+
+EXPOSE 7777
+
+RUN pip install -r /app/requirements.txt
+
+CMD ["python", "/app/docker_hello_world.py"]
---
+++ hello_world.py
@@ -0,0 +1,20 @@
+from tornado.web import Application
+from tornado.web import RequestHandler
+from tornado.ioloop import IOLoop
+
+
+class ChartHandler(RequestHandler):
+    def get(self):
+	params = self.get_arguments("user")
+	if params == []:
+		self.set_status(400)
+		return self.finish("Invalid user") 
+        self.write('Hello ' +self.get_argument("user"))
+
+if __name__ == "__main__":
+    handler_mapping = [
+                       (r'/hello', ChartHandler),
+                      ]
+    application = Application(handler_mapping)
+    application.listen(7777)
+    IOLoop.current().start()
